ddef56932ed7f3ae3f98f9901df56f4f0a22bf3c
Daniel Martins <daniel.ribamarmartins@hotmail.com>
2017-04-29 01:55:58 +0100

    angular init

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+var app = angular.module('web', []);
+app.controller('web_ctrl', function($scope) {
+    $scope.website = "W3B 0N TH3 R0CK5";
+    $scope.posts = [{
+            title: 'NodeJS Server',
+            description: 'Basic implementation of nodejs express',
+            gistid: '6123d7161631937d169d9d15f6187e78'
+        },
+        {
+            title: 'Umbraco',
+            description: 'Custom Create Content View',
+            gistid: '85611cc6a60bd02014f9a456b35fcc26'
+        }
+    ]
+});
