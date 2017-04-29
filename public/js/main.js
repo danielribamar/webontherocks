e86533f8f1bf2eeb100c00df61c2e3a95e2d3a58
Daniel Martins <daniel.ribamarmartins@hotmail.com>
2017-04-29 03:07:11 +0100

    upd

--- public/js/main.js
+++ public/js/main.js
@@ -18,6 +18,12 @@ app.controller('posts_ctrl', function($scope) {
             gistid: 'fa6f08bc71966ad98cf4565b44204d82'
         },
         {
+            title: 'Text',
+            description: 'Text Post',
+            text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            gistid: null
+        },
+        {
             title: 'Umbraco',
             description: 'Custom Create Content View',
             gistid: '85611cc6a60bd02014f9a456b35fcc26'
