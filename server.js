276c0f8f7b88769b1dc7b282a4020b73a3b5ff66
Daniel Martins <daniel.ribamarmartins@hotmail.com>
2017-04-29 01:08:52 +0100

    upd endpoint

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/blog', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/views/blog.html'));
 })
 
-app.listen(3000, function() {
+app.listen(5000, function() {
 
-    open('http://localhost:3000');
-    console.log('Example app listening on port 3000!')
+    open('http://localhost:5000');
+    console.log('Example app listening on port 5000!')
 })
