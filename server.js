7a791da73a4e15f77b7be3bdaf124bbe307e4dbd
Daniel Martins <daniel.ribamarmartins@hotmail.com>
2017-04-30 21:31:52 +0100

    upd: server

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
-app.get('/blog', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/views/blog.html'));
+app.get('/:postid', function(req, res) {
+    console.log(req.param('postid'));
+    res.sendFile(path.join(__dirname + '/public/views/post.html'));
 })
 
 app.listen(5000, function() {
-
     open('http://localhost:5000');
     console.log('Example app listening on port 5000!')
 })
