ebba9adcc70459c0fb6eab2177ec12fbc48c5a95
Daniel Martins <daniel.ribamarmartins@hotmail.com>
2017-05-01 01:06:05 +0100

    upd layout

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ var posts = [{
         title: 'NodeJS Server',
         description: 'Basic implementation of nodejs express',
         gistid: '6123d7161631937d169d9d15f6187e78',
-        text: ''
+        text: '',
+        languages: ['nodejs', 'javascript']
     },
     {
         id: 2,
         title: 'AngularJS',
         description: 'Basic Controllers and Directives',
         gistid: 'fa6f08bc71966ad98cf4565b44204d82',
-        text: ''
+        text: '',
+        languages: ['angularjs', 'javascript']
     },
     {
         id: 3,
@@ -23,14 +25,15 @@ var posts = [{
         description: 'Text Post',
         text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         gistid: null,
-
+        languages: ['dummy']
     },
     {
         id: 4,
         title: 'Umbraco',
         description: 'Custom Create Content View',
         gistid: '85611cc6a60bd02014f9a456b35fcc26',
-        text: ''
+        text: '',
+        languages: ['angularjs', 'html']
     }
 ];
 
@@ -44,9 +47,9 @@ app.get('/', function(req, res) {
 });
 
 app.get('/posts/:postid', function(req, res) {
-    var postid = req.param('postid');
+    var postid = req.params.postid;
     var post = findId(posts, postid)
-    res.render('post', { title: post.title, post: post })
+    res.render('post', { title: post.title, post: post, posts: posts })
 })
 
 app.listen(5000, function() {
