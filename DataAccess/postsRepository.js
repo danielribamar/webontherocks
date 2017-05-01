d4a653ad037774020573851e5e8723c6707321f9
Daniel Martins <daniel.ribamarmartins@hotmail.com>
2017-05-01 03:38:27 +0100

    get from database

---
+++ DataAccess/postsRepository.js
@@ -0,0 +1,30 @@
+var mongoClient = require('mongodb').MongoClient;
+var promise = require('promise');
+
+module.exports = {
+    getAll: function() {
+        return new Promise(function(resolve, reject) {
+            mongoClient.connect("mongodb://localhost:27017/webontherocks", function(err, db) {
+                if (!err || err == null) {
+                    db.collection('posts', function(err, collection) {
+                        collection.find().toArray(function(err, items) {
+                            resolve(items);
+                        });
+                    });
+                }
+                reject(err);
+            });
+        });
+
+    },
+    getById: function(id) {
+        mongoClient.connect("mongodb://localhost:27017/webontherocks", function(err, db) {
+            if (!err) {
+                console.log("We are connected");
+
+            }
+            console.log("Error connected");
+            return;
+        });
+    }
+};
--- server.js
+++ server.js
@@ -2,40 +2,8 @@ var express = require('express')
 var app = express()
 var open = require('open');
 var path = require('path');
-
-var posts = [{
-        id: 1,
-        title: 'NodeJS Server',
-        description: 'Basic implementation of nodejs express',
-        gistid: '6123d7161631937d169d9d15f6187e78',
-        text: '',
-        languages: ['nodejs', 'javascript']
-    },
-    {
-        id: 2,
-        title: 'AngularJS',
-        description: 'Basic Controllers and Directives',
-        gistid: 'fa6f08bc71966ad98cf4565b44204d82',
-        text: '',
-        languages: ['angularjs', 'javascript']
-    },
-    {
-        id: 3,
-        title: 'Text',
-        description: 'Text Post',
-        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        gistid: null,
-        languages: ['dummy']
-    },
-    {
-        id: 4,
-        title: 'Umbraco',
-        description: 'Custom Create Content View',
-        gistid: '85611cc6a60bd02014f9a456b35fcc26',
-        text: '',
-        languages: ['angularjs', 'html']
-    }
-];
+var postsRepository = require('./DataAccess/postsRepository.js');
+var promise = require('promise');
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/public/views')
@@ -43,7 +11,19 @@ app.set('views', __dirname + '/public/views')
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.render('homepage', { title: 'W3B 0N TH3 R0CK5', year: new Date().getFullYear(), posts: posts })
+
+    postsRepository.getAll()
+        .then(result => {
+            console.log('will render now');
+            res.render('homepage', {
+                title: 'W3B 0N TH3 R0CK5',
+                year: new Date().getFullYear(),
+                posts: result
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 app.get('/posts/:postid', function(req, res) {
@@ -59,7 +39,7 @@ app.listen(5000, function() {
 
 function findId(data, idToLookFor) {
     for (var i = 0; i < data.length; i++) {
-        if (data[i].id == idToLookFor) {
+        if (data[i].ObjectId == idToLookFor) {
             return (data[i]);
         }
     }
