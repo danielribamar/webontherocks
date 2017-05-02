a635316f194a90bf0b6bdf0b8f6bd71c30d57066
DANIELMARTINS-P\daniel.martins <daniel.martins@tribe.pt>
2017-05-02 14:51:24 +0100

    UPD: packages

--- DataAccess/postsRepository.js
+++ DataAccess/postsRepository.js
@@ -1,11 +1,13 @@
 var mongoClient = require('mongodb').MongoClient;
 var promise = require('promise');
 var ObjectId = require('mongodb').ObjectID;
+
 module.exports = {
 
     getAll: function() {
         return new Promise(function(resolve, reject) {
-            mongoClient.connect("mongodb://localhost:27017/webontherocks", function(err, db) {
+            mongoClient.connect("mongodb://192.168.3.156:27017/webontherocks", function(err, db) {
+                // mongoClient.connect("mongodb://localhost:27017/webontherocks", function(err, db) {
                 if (!err || err == null) {
                     db.collection('posts', function(err, collection) {
                         collection.find().toArray(function(err, items) {
@@ -22,7 +24,8 @@ module.exports = {
     },
     getById: function(id) {
         return new Promise(function(resolve, reject) {
-            mongoClient.connect("mongodb://localhost:27017/webontherocks", function(err, db) {
+            mongoClient.connect("mongodb://192.168.3.156:27017/webontherocks", function(err, db) {
+                // mongoClient.connect("mongodb://localhost:27017/webontherocks", function(err, db) {
                 if (!err || err == null) {
                     var item = db.collection('posts').findOne({ _id: ObjectId(id) })
                     resolve(item);
